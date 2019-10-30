8e5f2c14dc444a8a472ae43db7a1ae7f296f2d6c
Kevin Poston <kevposton@gmail.com>
2019-10-29 23:42:04 -0400

    adding results

---
+++ components/FlashCardResult.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="result">
+    <span class="answer" v-bind:class="{ correct: status.correct, wrong: !status.correct }">{{ status.answer }}</span>
+    <span class="correct result-sub" v-if="!status.correct">Correct Answer: {{ status.equation.answer }}</span>
+    <span class="correct result-sub" v-if="status.correct">Nice Job!</span>
+    <button v-on:click="$emit('next-equation')" class="btn">Next</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['status']
+}
+</script>
+
+<style>
+.result {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.result-sub {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.result > .answer {
+  font-family: 'Luckiest Guy', cursive;
+  font-size: 10rem;
+  margin-top: auto;
+}
+
+.correct {
+  color: #89e58d;
+}
+
+.wrong {
+  color: #ff5a4e;
+}
+
+.result > .btn {
+  margin-top: auto;
+}
+</style>
--- components/FlashCards.vue
+++ components/FlashCards.vue
@@ -1,35 +1,71 @@
 <template>
   <section class="container">
-    <FlashCard v-on:next-equation="getNextEquation" :equation='equations[id]'/>
+    <div class="card">
+      <FlashCardQuestion v-if="this.askingQuestion" v-on:question-answered="questionAnswered" :equation='equations[currentQuestionId]'/>
+      <FlashCardResult v-else-if="!this.done" v-on:next-equation="getNextEquation" :status="this.currentQuestionStatus" />
+      <div v-else>More Coming Soon Mackenzie!</div>
+    </div>
   </section>
 </template>
-
 <script>
-import FlashCard from "~/components/FlashCard.vue";
+import FlashCardQuestion from "~/components/FlashCardQuestion.vue";
+import FlashCardResult from "~/components/FlashCardResult.vue";
 
 export default {
     components: {
-    FlashCard
+    FlashCardQuestion,
+    FlashCardResult
   },
   props: ['equations'],
   data: function() {
     return {
-      id: 0,
-      results: []
+      currentQuestionId: 0,
+      askingQuestion: true,
+      currentQuestionStatus: {
+        equation: {},
+        answer: 0,
+        correct: false
+      },
+      results: [],
+      done: false
     };
   },
   methods: {
-    getNextEquation: function(previousResult) {
-      this.results.push({
-        ...this.equations[this.id],
-        correct: previousResult
-      });
-      this.id++;
+    questionAnswered: function(answer) {
+      const equation = this.equations[this.currentQuestionId];
+      const correct = equation.answer == answer;
+      this.currentQuestionStatus = {
+        equation,
+        answer,
+        correct
+      };
+      this.results.push(this.currentQuestionStatus);
+      this.askingQuestion = false;
+    },
+    getNextEquation: function() {
+      this.currentQuestionId++;
+      
+      if (this.currentQuestionId < this.equations.length)
+        this.askingQuestion = true;
+      else
+        this.done = true;
     }
   }
 }
 </script>
 
 <style>
-
+.card {
+  background: #fff;
+  border-radius: 2px;
+  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  color: #5244c2;
+  display: inline-block;
+  font-size: 3em;
+  height: 400px;
+  margin: .5rem;
+  padding: 2rem;
+  position: relative;
+  width: 300px;
+}
 </style>
