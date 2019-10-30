7914b7f99964e0a1c2c55dbcd2a025cd69494bbc
Kevin Poston <kevposton@gmail.com>
2019-10-29 23:42:04 -0400

    adding results

---
+++ components/FlashCardQuestion.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="equation">
+      <div></div>
+      <div>{{equation.topNumber}}</div>
+      <div class="operator">{{equation.operator}}</div>
+      <div>{{equation.bottomNumber}}</div>
+    </div>
+    <div class="container-vertical answer">
+      <input v-model="answer" type="number">
+      <button v-on:click="$emit('question-answered', answer)" class="btn">Answer</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['equation'],
+  data: function() {
+    return {
+      answer: ''
+    }
+  }
+}
+</script>
+
+<style>
+.equation {
+  border-bottom: 10px solid #5244c2;
+  font-family: 'Luckiest Guy', cursive;
+  margin: 20px auto;
+  width: 100%;
+  display:grid;
+  grid-template-columns: 1fr 2fr;
+  grid-row: auto auto;
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+}
+
+.operator {
+  display: flex;
+  justify-self: end;
+}
+
+.answer {
+  height: 150px;
+}
+
+.result {
+  height: 300px;
+}
+
+.answer > button,
+.result > button {
+  margin-top: auto;
+}
+
+.answer > input {
+  border: none;
+  color: #5244c2;
+  font-family: 'Luckiest Guy', cursive;
+  font-size: 3rem;
+  padding-top: 1rem;
+  padding-right: 1rem;
+  text-align: right;
+}
+</style>
